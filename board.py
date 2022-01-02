a83d887711b747d00e59195152fa5ef63021e9f8
TheTerrior <werbird10@gmail.com>
2022-01-02 11:45:32 -0800

    Continued with win detection implementation.

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 class Board():
     def __init__(self):
-        self.matrix = [ [ Tile() for _ in range(5) ] for _ in range(6) ]
+        self.matrix = [ [ Tile((x, y)) for y in range(5) ] for x in range(6) ]
+        self.win = False
 
     '''
     Drop a piece onto the board. Accepts a column (integer) and a color (either "RED" or "YELLOW").
@@ -23,18 +24,41 @@ class Board():
             __update_new_tile(coords)
         return True
 
-    def __update_new_tile(self, coords):
-        pass
+    '''
+    def __adjacent_generator(self, coords):
+        yield 0, matrix[coords[0]][coords[1] + 1], matrix[coords[0]][coords[1] - 1]
+        yield 1, matrix[coords[0] + 1][coords[1] + 1], matrix[coords[0] - 1][coords[1] - 1]
+        yield 2, matrix[coords[0] + 1][coords[1]], matrix[coords[0] - 1][coords[1]]
+        yield 3, matrix[coords[0] - 1][coords[1] + 1], matrix[coords[0] + 1][coords[1] - 1]
 
-    def __update_tiles(self, coords, direction):
+    def __update_new_tile(self, coords):
+        cur = matrix[coords[0]][coords[1]]
+        for direction, checkpos, checkneg, coords in __adjacent_generator(coords):
+            cur.adjacent[direction] = ((checkpos.adjacent if checkpos.color == cur.color else 0) +
+                (checkneg.adjacent if checkneg.color == cur.color else 0) + 1)
+            if checkpos.color == cur.color:
+                __update_tiles(checkpos.color)
+            if checkneg.color == cur.color:
+                __update_tiles(checkneg.color)
+        ''
+        cur.adjacent = ((checkpos.adjacent if checkpos.color == cur.color else 0) +
+            (checkneg.adjacent if checkneg.color == cur.color else 0) + 1)
+        ''
+    '''
+
+    def __update_new_tile(coords):
+        cur = matrix[coords[0]][coords[1]]
+        
+    def __update_tiles(self, tile, direction, checked):
         pass
 
 class Tile():
 
-    def __init__(self, color = "BLANK"):
+    def __init__(self, coords, color = "BLANK"):
         if color in ["BLANK", "RED", "YELLOW"]:
             self.color = color
-            self.adjacent = 0
+            self.adjacent = [0, 0, 0, 0]
+            self.coords = coords
         else:
             print("Warning: The provided color was invalid. Defaulting to \"BLANK\"")
 
