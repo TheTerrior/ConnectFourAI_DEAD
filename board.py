2f4270870d1bc506c5450764aaa265a9790a2e05
TheTerrior <werbird10@gmail.com>
2022-01-05 23:28:57 -0800

    Added changes to allow for tie checking, as well as a stop for gameplay if a win or tie occurs.

--- board.py
+++ board.py
@@ -1,7 +1,8 @@
 class Board():
     def __init__(self):
         self.matrix = [ [ "BLANK" for _ in range(6) ] for _ in range(7) ]
-        self.win = False
+        self.win = 0
+        self.cols_open = 7
 
     '''
     Drop a piece onto the board. Accepts a column (integer) and a color (either "RED" or "YELLOW").
@@ -17,27 +18,30 @@ class Board():
             raise ValueError("'drop' given invalid color!")
     
     def __place(self, coords, color):
-        print(coords)
-        if coords[1] < 0 or self.matrix[coords[0]][coords[1]] != "BLANK":
+        if coords[1] < 0 or self.matrix[coords[0]][coords[1]] != "BLANK" or self.win > 0:
             return False
         if not self.__place((coords[0], coords[1] - 1), color):
             self.matrix[coords[0]][coords[1]] = color
+            if coords[1] == 5:
+                self.cols_open -= 1
+                if self.cols_open == 0:
+                    self.win = 2
             self.__win_detect(coords)
         return True
 
     def __win_detect(self, coords):
         color = self.matrix[coords[0]][coords[1]]
         if self.__win_recursive((coords[0] + 1, coords[1]), 0, color) + self.__win_recursive((coords[0] - 1, coords[1]), 4, color) > 2:
-            self.win = True
+            self.win = 1
             return
         if self.__win_recursive((coords[0] + 1, coords[1] + 1), 1, color) + self.__win_recursive((coords[0] - 1, coords[1] - 1), 5, color) > 2:
-            self.win = True
+            self.win = 1
             return
         if self.__win_recursive((coords[0], coords[1] + 1), 2, color) + self.__win_recursive((coords[0], coords[1] - 1), 6, color) > 2:
-            self.win = True
+            self.win = 1
             return
         if self.__win_recursive((coords[0] - 1, coords[1] + 1), 3, color) + self.__win_recursive((coords[0] + 1, coords[1] - 1), 7, color) > 2:
-            self.win = True
+            self.win = 1
         
     def __win_recursive(self, coords, direction, color):
         if coords[0] < 0 or coords[0] > 6 or coords[1] < 0 or coords[1] > 5:
